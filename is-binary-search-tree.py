74a1d43d834670d91ba20232908e253bb4d394b4
babangsund <babangsund@gmail.com>
2019-10-12 00:38:45 +0200

    prettier bst validation

--- is-binary-search-tree.py
+++ is-binary-search-tree.py
@@ -1,22 +1,15 @@
 """ https://www.hackerrank.com/challenges/is-binary-search-tree/problem """
 
 
+# DFS traversal, O(n) runtime, O(h) space
 def check_binary_search_tree_(root, min=None, max=None):
     if root is None:
         return True
 
-    data = root.data
-
-    if min is not None and data <= min:
+    if (min is not None and root.data <= min) or (max is not None and root.data >= max):
         return False
 
-    if max is not None and data >= max:
-        return False
+    is_left_valid = check_binary_search_tree_(root.left, min, root.data)
+    is_right_valid = check_binary_search_tree_(root.right, root.data, max)
 
-    if root.left is not None and not check_binary_search_tree_(root.left, min, data):
-        return False
-
-    if root.right is not None and not check_binary_search_tree_(root.right, data, max):
-        return False
-
-    return True
+    return is_left_valid and is_right_valid
--- validate-binary-search-tree.js
+++ validate-binary-search-tree.js
@@ -1,25 +1,17 @@
 // https://leetcode.com/problems/validate-binary-search-tree
 
-var isValidBST = function(root, min = null, max = null) {
+// DFS traversal, O(n) runtime, O(h) space
+function isValidBST(root, min = null, max = null) {
   if (!root) {
     return true;
   }
 
-  if (min !== null && root.val <= min) {
+  if ((min !== null && root.val <= min) || (max !== null && root.val >= max)) {
     return false;
   }
 
-  if (max !== null && root.val >= max) {
-    return false;
-  }
-
-  if (root.left && !isValidBST(root.left, min, root.val)) {
-    return false;
-  }
-
-  if (root.right && !isValidBST(root.right, root.val, max)) {
-    return false;
-  }
-
-  return true;
-};
+  return (
+    isValidBST(root.left, min, root.val) &&
+    isValidBST(root.right, root.val, max)
+  );
+}
