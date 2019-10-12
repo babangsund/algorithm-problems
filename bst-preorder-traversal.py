58316f58cfab3e9a00ca8130fabb02d7246b2e58
babangsund <babangsund@gmail.com>
2019-10-12 20:56:27 +0200

    bst-preorder-traversal.py

---
+++ bst-preorder-traversal.py
@@ -0,0 +1,15 @@
+""" https://www.hackerrank.com/challenges/tree-preorder-traversal/problem """
+
+
+def pre_order(root):
+    if root is None:
+        return ""
+
+    print(rec(root))
+
+
+def rec(root):
+    if root is None:
+        return ""
+
+    return " ".join(str(s) for s in [root.info, rec(root.left), rec(root.right)] if s)
