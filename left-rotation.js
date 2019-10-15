4ef55fdfba8f17554c2cc83f47cf24a12dad35b8
babangsund <babangsund@gmail.com>
2019-10-15 23:42:53 +0200

    update left-rotation.js comments

--- left-rotation.js
+++ left-rotation.js
@@ -1,16 +1,4 @@
-// A left rotation operation on an array shifts each of the array's elements 1 unit to the left.
-// For example, if 2 left rotations are performed on array [1,2,3,4,5], then the array would become [3,4,5,1,2].
-
-/**
- * Given [1,2,3,4,5], 2
- * return [3,4,5,1,2]
- */
-
-/**
- * @param {number[]} a
- * @param {number} d
- * return {number[]}
- */
+// https://www.hackerrank.com/challenges/array-left-rotation/problem
 
 function leftRotation(a, d) {
   const length = a.length;
@@ -24,5 +12,5 @@ function leftRotation(a, d) {
   return result;
 }
 
-// Runtime is O(a)
+// Runtime: O(a)
 // Space is O(a)
