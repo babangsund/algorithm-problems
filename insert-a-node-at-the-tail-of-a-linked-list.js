04cfa04634923eb725efd508db8cf87f7bdaece9
babangsund <babangsund@gmail.com>
2019-10-15 20:42:48 +0200

    insert-a-node-at-the-tail-of-a-linked-list.js

---
+++ insert-a-node-at-the-tail-of-a-linked-list.js
@@ -0,0 +1,15 @@
+// https://www.hackerrank.com/challenges/insert-a-node-at-the-tail-of-a-linked-list/problem
+
+function insertNodeAtTail(head, data) {
+  const tail = new SinglyLinkedListNode(data);
+  if (!head) return tail;
+
+  let next = head;
+
+  while (next.next) {
+    next = next.next;
+  }
+
+  next.next = tail;
+  return head;
+}
