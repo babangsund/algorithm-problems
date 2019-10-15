d95d6e6ae731b148e4f3c675746076346a76d514
babangsund <babangsund@gmail.com>
2019-10-16 00:31:48 +0200

    delete-duplicate-value-nodes-from-a-sorted-linked-list.js w/o recurse

--- delete-duplicate-value-nodes-from-a-sorted-linked-list.js
+++ delete-duplicate-value-nodes-from-a-sorted-linked-list.js
@@ -1,14 +1,18 @@
 // https://www.hackerrank.com/challenges/delete-duplicate-value-nodes-from-a-sorted-linked-list/problem
 
 function removeDuplicates(head) {
-  if (!head) return null;
+  if (!head || !head.next) return head;
 
-  let next = head.next;
-  while (next && head.data === next.data) {
-    next = next.next;
+  let next = head;
+
+  while (next.next) {
+    if (next.data !== next.next.data) {
+      next = next.next;
+    } else {
+      next.next = next.next.next;
+    }
   }
 
-  head.next = removeDuplicates(next);
   return head;
 }
 
