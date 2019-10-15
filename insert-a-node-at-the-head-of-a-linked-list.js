bf10fc4897f3373b98f50bc2f9c105f795708a85
babangsund <babangsund@gmail.com>
2019-10-15 22:10:48 +0200

    insert-a-node-at-the-head-of-a-linked-list.js

---
+++ insert-a-node-at-the-head-of-a-linked-list.js
@@ -0,0 +1,7 @@
+// https://www.hackerrank.com/challenges/insert-a-node-at-the-head-of-a-linked-list/problem
+
+function insertNodeAtHead(head, data) {
+  const node = new SinglyLinkedListNode(data);
+  node.next = head;
+  return node;
+}
