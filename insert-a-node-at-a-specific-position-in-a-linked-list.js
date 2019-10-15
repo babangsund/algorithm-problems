dffdae7ba5e29680b3f77696d05aeddb789a89df
babangsund <babangsund@gmail.com>
2019-10-15 23:44:24 +0200

    break instead of bool

--- insert-a-node-at-a-specific-position-in-a-linked-list.js
+++ insert-a-node-at-a-specific-position-in-a-linked-list.js
@@ -1,16 +1,15 @@
 // https://www.hackerrank.com/challenges/insert-a-node-at-a-specific-position-in-a-linked-list/problem
 
 function insertNodeAtPosition(head, data, position) {
-  let done = false;
   let next = head;
   let currentPosition = 0;
 
-  while (next && !done) {
+  while (next) {
     if (currentPosition === position - 1) {
       const node = new SinglyLinkedListNode(data);
       node.next = next.next;
       next.next = node;
-      done = true;
+      break;
     }
     currentPosition++;
     next = next.next;
