58b26f1533a6ab72a25699dfcf8a5c2f553c4fbe
babangsund <babangsund@gmail.com>
2019-10-16 10:13:24 +0200

    find-the-merge-point-of-two-joined-linked-lists.js

---
+++ find-the-merge-point-of-two-joined-linked-lists.js
@@ -0,0 +1,63 @@
+// https://www.hackerrank.com/challenges/find-the-merge-point-of-two-joined-linked-lists/problem
+
+// Solution #1
+
+function findMergeNode(headA, headB) {
+  let nextA = headA;
+  let nextB = headB;
+
+  while (nextA !== nextB) {
+    if (nextA.next) nextA = nextA.next;
+    else nextA = headB;
+
+    if (nextB.next) nextB = nextB.next;
+    else nextB = headA;
+  }
+
+  return nextB.data;
+}
+// Runtime: O(n)
+// Space: Constant
+
+// Solution #2
+function llToMap(head) {
+  const map = new Map();
+  let next = head;
+
+  while (next) {
+    map.set(next, next);
+    next = next.next;
+  }
+
+  return map;
+}
+
+function findMergeNode(headA, headB) {
+  const mapA = llToMap(headA);
+
+  let nextB = headB;
+  while (nextB) {
+    if (mapA.has(nextB)) return nextB.data;
+    nextB = nextB.next;
+  }
+}
+// Runtime: O(a + b)
+// Space: O(a)
+
+// Solution #3
+function findMergeNode(headA, headB) {
+  let nextA = headA;
+  while (nextA) {
+    nextA.visited = true;
+    nextA = nextA.next;
+  }
+
+  let nextB = headB;
+  while (!nextB.visited) {
+    nextB = nextB.next;
+  }
+
+  return nextB.data;
+}
+// Runtime: O(a + b)
+// Space: Constant
