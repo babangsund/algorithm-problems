f166953c2863971249cd0a49a99d701c52f28fb2
babangsund <babangsund@gmail.com>
2019-10-20 22:17:37 +0200

    Remove redundant diff variable

--- valid-parentheses.js
+++ valid-parentheses.js
@@ -5,28 +5,24 @@ function isValid(word) {
   if (len % 2 !== 0) return false;
 
   const closing = [];
-  let diff = 0;
-
   const open = ['(', '[', '{'];
   const close = [')', ']', '}'];
 
   for (const str of word) {
     if (open.includes(str)) {
-      diff--;
       if (str === '(') closing.push(')');
       if (str === '[') closing.push(']');
       if (str === '{') closing.push('}');
     }
 
     if (close.includes(str)) {
-      diff++;
       if (str === ')' && closing.pop() !== ')') return false;
       if (str === ']' && closing.pop() !== ']') return false;
       if (str === '}' && closing.pop() !== '}') return false;
     }
   }
 
-  return diff === 0 ? true : false;
+  return closing.length === 0 ? true : false;
 }
 
 // Runtime: O(n), n = word.length;
