3aebc90e86121b9f8d32ba346b9c4066b0f3b565
babangsund <babangsund@gmail.com>
2019-10-26 17:43:42 -0700

    Recursive BS

--- binary-search.js
+++ binary-search.js
@@ -17,3 +17,18 @@ function search(nums, target) {
 
 // Runtime: O(log(n))
 // Space: O(1)
+
+/* Recursive */
+function search(nums, target) {
+  return searchHelper(nums, target, nums.length / 2, 0, nums.length - 1);
+}
+
+function searchHelper(nums, target, mid, from, to) {
+  if (target === nums[mid]) return mid;
+  if (!(from <= to)) return -1;
+
+  mid = Math.floor((to - from) / 2) + from;
+
+  if (target > nums[mid]) return searchHelper(nums, target, mid, mid + 1, to);
+  else return searchHelper(nums, target, mid, from, mid - 1);
+}
